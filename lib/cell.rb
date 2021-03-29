5b3719e93eabe3a66a9a7f5334f102cf14726323
Lawrence Whalen <ldwhalen@gmail.com>
2021-03-29 16:04:11 -0600

    Red green empty? and place_ship Cell methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,18 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate,
+              :ship
 
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
+  end
+
+  def empty?
+    @ship == nil
+  end
+
+  def place_ship(ship)
+    @ship = ship
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,16 +4,32 @@ require './lib/cell'
 RSpec.describe do
   describe '#initialize' do
     it 'creates instance of Cell' do
-      cell = Cell.new("A1")
+      cell = Cell.new('A1')
 
       expect(cell).to be_instance_of(Cell)
     end
     it 'starts without an ship' do
-      cell = Cell.new("A1")
+      cell = Cell.new('A1')
 
-      expect(cell.ship).to eq(nil) 
+      expect(cell.ship).to eq(nil)
     end
   end
 
+  describe '#empty?' do
+    it 'returns true when empty' do
+      cell = Cell.new('A1')
 
+      expect(cell.empty?).to eq(true)
+    end
+  end
+  describe '#place_ship' do
+    it 'places the a ship in the cell' do
+      cruiser = Ship.new('Cruiser', 3)
+      cell = Cell.new('A1')
+
+      cell.place_ship(cruiser)
+
+      expect(cell.ship).to eq(cruiser)
+    end
+  end
 end
