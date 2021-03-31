86b6dea0a6b30dde9ae6bb03f812d6ada4a3a087
Lawrence Whalen <ldwhalen@gmail.com>
2021-03-30 19:12:23 -0600

    Create board hash in intialize

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -17,5 +17,14 @@ RSpec.describe do
 
       expect(cell_hash.class).to eq(Hash)
     end
+
+    it 'returns 16 key value pairs' do
+      board = Board.new
+
+      cell_hash = board.cells
+
+      expect(cell_hash.keys.count).to eq(16)
+      expect(cell_hash.values.count).to eq(16)
+    end
   end
 end
